c761c9f7f17ece7bcf73f66889f4724e80d0aff8
Ashley Seerattan <aseerattan@ggc.edu>
2023-02-02 17:08:19 -0500

    completed

---
+++ src/FindX.java
@@ -0,0 +1,16 @@
+/**
+ * @Class name: Find X
+ * @Author: Ashley Seerattan
+ * @Version: 1.0
+ * @Date 01 February 2023
+ * Description: Find X
+public class FindX {
+ Public static void main (String args[] {
+  int x;
+  int y;
+// 5+19+x+47=194
+int x = 194-47-19-5;
+System.out.println("Value of X + " + x);
+
+
+}
