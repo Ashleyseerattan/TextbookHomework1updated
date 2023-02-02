2631538f0a6d612568d8ab25af40a63a87a0c3e5
Ashley Seerattan <aseerattan@ggc.edu>
2023-02-02 15:47:55 -0500

    edited

--- src/Chocolate.java
+++ src/Chocolate.java
@@ -6,12 +6,24 @@
 
  * Description: Chocolate
  */
-
-
-
-
-
-
-
 public class Chocolate {
-}
+    public static void main(String[]args){
+      int total_bag = 9;
+      int each_bag = 2;
+      int total_size = 3;
+      System.out.print( total_bag * each_bag);
+      int new_each_bag = 3;
+      System.out.print("total_chcolates"/new_each_bag);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
---
+++ src/Stamps.java
@@ -0,0 +1,9 @@
+/**
+ * @Class name: Stamps
+ * @Author: Ashley Seerattan
+ * @Version: 1.0
+ * @Date 01 February 2023
+ * Description: : Stamps
+ */
+public class Stamps {
+}
