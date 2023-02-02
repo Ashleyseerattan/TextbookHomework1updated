7bbd78bc65e3d926bac3642a63ceb1e5d7a44554
Ashley Seerattan <aseerattan@ggc.edu>
2023-02-02 15:14:56 -0500

    Completed

--- src/SpeedLight.java
+++ src/SpeedLight.java
@@ -3,18 +3,20 @@
  * @Author: Ashley Seerattan
  * @Version: 1.0
  * @Date 01 February 2023
- * Description: A program designed to calculate the speed of light
+ * Description: this program will write SpeedLight that calculate the
+ * distance to a lightening strick based on the time elapsed between the flash
+ * and the sound of thunder.
+ * In this program, I will declare variables: speedOfSound, timeElapsed,
+ * distance
  */
-
-
-
-
-
-
-
 public class SpeedLight {
     public static void main(String[]args){
-       //edited
+       int SPEED_OF_SOUND = 340;
+       int timeElapsed = 5;
+       int distance = SPEED_OF_SOUND * timeElapsed;
+       System.out.print("Speed of sound:" + SPEED_OF_SOUND);
+       System.out.println("Time Elapsed: " + timeElapsed);
+       System.out.println("Distance:" + distance);
 
     }
 }
