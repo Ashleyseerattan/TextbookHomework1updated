7714c7706e2fb6d4c05f651c748191cff2330c93
Ashley Seerattan <aseerattan@ggc.edu>
2023-02-02 17:10:58 -0500

    completed

---
+++ src/MaleStudent.java
@@ -0,0 +1,11 @@
+/**
+ * @Class name: Find Male students
+ * @Author: Ashley Seerattan
+ * @Version: 1.0
+ * @Date 01 February 2023
+ * Description: Fnd how many students are male
+public class MaleStudent {
+    public static void main(String[] args) {
+        int total = 389, girls = 175, boys;
+        System.out.printf("total - girls);
+    }
