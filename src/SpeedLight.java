c4acb134631f7a67fdc758e0b34e85d75adf5daa
Ashley Seerattan <aseerattan@ggc.edu>
2023-02-02 14:33:15 -0500

    added commented line.

--- src/SpeedLight.java
+++ src/SpeedLight.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class SpeedLight {
+/**
+ * @Class name: Speed light
+ * @Author: Ashley Seerattan
+ * @Version: 1.0
+ * @Date 01 February 2023
+ * Description: A program designed to calculate the speed of light
+ */
+
+
+
+
+
+
+
+public class SpeedLight {
+    public static void main(String[]args){
+       //complete here
+
+    }
 }
